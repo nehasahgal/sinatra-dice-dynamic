19252db8d354366fdcb0439b5e78d8f832f96de3
nehasahgal <146212872+nehasahgal@users.noreply.github.com>
2023-10-18 14:08:58 +0000

    remove old code

--- app.rb
+++ app.rb
@@ -5,54 +5,6 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/2/6") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..6)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_six)
-end
-
-get("/dice/2/10") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..10)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_ten)
-end
-
-get("/dice/1/20") do
-  @rolls = []
-
-  1.times do
-    die = rand(1..20)
-
-    @rolls.push(die)
-  end
-
-  erb(:one_twenty)
-end
-
-get("/dice/5/4") do
-  @rolls = []
-
-  5.times do
-    die = rand(1..4)
-
-    @rolls.push(die)
-  end
-
-  erb(:five_four)
-end
-
 get("/dice/:dice/:sides") do #make "zebra" a wildcard using a colon
   @number_of_dice = params.fetch("dice").to_i
   @number_of_sides = params.fetch("sides").to_i
